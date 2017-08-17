a24ab3b1db7e7eb0093f829848a14f87f925250c
Matt Gucci <matt9ucci@gmail.com>
2017-08-17 23:37:56 +0900

    ModuleVersion = '0.12.2.170823'
    Add completer for:
    * create_--engine-env
    * create --virtualbox-ui-type
    * env --shell
    * help

--- DockerMachineCompletion/completers4arguments.ps1
+++ DockerMachineCompletion/completers4arguments.ps1
@@ -17,7 +17,24 @@ $machineName = {
 
 Register-Completer 'docker-machine_config' $machineName
 Register-Completer 'docker-machine_create' $machineName
+Register-Completer 'docker-machine_create_--engine-env' {
+	"HTTP_PROXY=$env:HTTP_PROXY"
+	"HTTPS_PROXY=$env:HTTPS_PROXY"
+	"NO_PROXY=$env:NO_PROXY"
+	"http_proxy=$env:http_proxy"
+	"https_proxy=$env:https_proxy"
+	"no_proxy=$env:no_proxy"
+}
+Register-Completer 'docker-machine_create_--virtualbox-ui-type' { 'gui', 'sdl', 'headless', 'separate' }
 Register-Completer 'docker-machine_env' $machineName
+Register-Completer 'docker-machine_env_--shell' {
+	'cmd'
+	'emacs'
+	'fish'
+	'powershell'
+	'tcsh'
+}
+Register-Completer 'docker-machine_help' (Get-Completer 'docker-machine')
 Register-Completer 'docker-machine_inspect' $machineName
 Register-Completer 'docker-machine_ip' $machineName
 Register-Completer 'docker-machine_kill' $machineName
