d264399380b68f211c6568b0c11037989b07e518
Matt Gucci <matt9ucci@gmail.com>
2019-01-28 22:27:38 +0900

    Add Tests

---
+++ Tests/DockerMachineCompletion.Tests.ps1
@@ -0,0 +1,40 @@
+Import-Module -Name $PSScriptRoot\..\DockerMachineCompletion -Force
+
+function Get-Result([string]$Text, [int]$CursorPosition = $Text.Length, [hashtable]$Options) {
+	[System.Management.Automation.CommandCompletion]::CompleteInput($Text, $CursorPosition, $Options).CompletionMatches
+}
+
+Describe 'docker-machine' {
+	$result = Get-Result 'docker-machine --t'
+	$result.Count | Should Be 4
+	$result[0].CompletionText | Should Be --tls-ca-cert
+	$result[1].CompletionText | Should Be --tls-ca-key
+	$result[2].CompletionText | Should Be --tls-client-cert
+	$result[3].CompletionText | Should Be --tls-client-key
+
+	$result = Get-Result 'docker-machine st'
+	$result.Count | Should Be 3
+	$result[0].CompletionText | Should Be start
+	$result[1].CompletionText | Should Be status
+	$result[2].CompletionText | Should Be stop
+}
+
+Describe 'docker-machine create' {
+	$result = Get-Result 'docker-machine create --e'
+	$result.Count | Should Be 7
+	$result[0].CompletionText | Should Be --engine-env
+	$result[1].CompletionText | Should Be --engine-insecure-registry
+	$result[2].CompletionText | Should Be --engine-install-url
+	$result[3].CompletionText | Should Be --engine-label
+	$result[4].CompletionText | Should Be --engine-opt
+	$result[5].CompletionText | Should Be --engine-registry-mirror
+	$result[6].CompletionText | Should Be --engine-storage-driver
+}
+
+Describe 'docker-machine create -d azure' {
+	$result = Get-Result 'docker-machine create -d azure --azure-c'
+	$result.Count | Should Be 3
+	$result[0].CompletionText | Should Be --azure-client-id
+	$result[1].CompletionText | Should Be --azure-client-secret
+	$result[2].CompletionText | Should Be --azure-custom-data
+}
